b88b9738f2e271e655269fa441385244b35332c7
Roman Gafurov <gafurov.roma89@gmail.com>
2019-05-22 10:45:27 +0300

    week 1 homework

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -3,10 +3,104 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
+	"path"
 )
+
+type entry struct {
+	isDir  bool
+	isLast bool
+	path   string
+	prefix string
+	info   os.FileInfo
+}
+
+func readDirEntries(root string, prefix string, withFiles bool) (entries []*entry, err error) {
+	infos, err := ioutil.ReadDir(root)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, info := range infos {
+		isDir := info.IsDir()
+
+		if !(isDir || withFiles) {
+			continue
+		}
+
+		entries = append(entries, &entry{
+			info:   info,
+			isDir:  isDir,
+			prefix: prefix,
+			path:   path.Join(root, info.Name()),
+		})
+	}
+
+	if ln := len(entries); ln != 0 {
+		entries[ln-1].isLast = true
+	}
+
+	return
+}
+
+func printEntry(out io.Writer, entry *entry) {
+	fmt.Fprint(out, entry.prefix)
+
+	if entry.isLast {
+		fmt.Fprint(out, "└───")
+	} else {
+		fmt.Fprint(out, "├───")
+	}
+
+	fmt.Fprint(out, entry.info.Name())
+
+	if entry.isDir {
+		// skip
+	} else if size := entry.info.Size(); size != 0 {
+		fmt.Fprintf(out, " (%db)", size)
+	} else {
+		fmt.Fprint(out, " (empty)")
+	}
+
+	fmt.Fprint(out, "\n")
+}
+
+func dirTree(out io.Writer, root string, printFiles bool) error {
+	var prefix string
+	entries, err := readDirEntries(root, "", printFiles)
+
+	if err != nil {
+		return err
+	}
+
+	for len(entries) != 0 {
+		entry, rest := entries[0], entries[1:]
+
+		if entry.isDir {
+			if entry.isLast {
+				prefix = "\t"
+			} else {
+				prefix = "│\t"
+			}
+
+			childs, err := readDirEntries(entry.path, entry.prefix+prefix, printFiles)
+
+			if err != nil {
+				return err
+			}
+
+			entries = append(childs, rest...)
+		} else {
+			entries = rest
+		}
+
+		printEntry(out, entry)
+	}
+
+	return nil
+}
 
 func main() {
 	out := os.Stdout
