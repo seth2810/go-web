652837f00a7161868cd706fbce649ea520797f65
Roman Gafurov <gafurov.roma89@gmail.com>
2019-05-23 13:40:59 +0300

    week 3 homework

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -1,10 +1,141 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"os"
+	"strings"
+
+	json "encoding/json"
+
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
 )
 
+// User ...
+// easyjson:json
+type User struct {
+	Name     string   `json:"name"`
+	Email    string   `json:"email"`
+	Browsers []string `json:"browsers"`
+}
+
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+func easyjson3486653aDecodeGoWebHw3Bench(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "name":
+			out.Name = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson3486653aDecodeGoWebHw3Bench(&r, v)
+	return r.Error()
+}
+
+// FastSearch ...
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	seenBrowsers := make(map[string]bool, 120)
+
+	fmt.Fprintln(out, "found users:")
+
+	for idx := 0; scanner.Scan(); idx++ {
+		user := &User{}
+
+		err := user.UnmarshalJSON(scanner.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		isMSIE := false
+		isAndroid := false
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				seenBrowsers[browser] = true
+			} else if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				seenBrowsers[browser] = true
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		fmt.Fprintf(out, "[%d] %s <%s>", idx, user.Name, strings.Replace(user.Email, "@", " [at] ", 1))
+		fmt.Fprintln(out)
+	}
+
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
